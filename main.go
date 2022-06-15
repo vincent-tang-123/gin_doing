d7ef65b7aba1378c93b1aabffbe663009abc8920
唐 <vincent@tangdeMacBook-Pro.local>
2022-06-15 15:07:49 +0800

    gorm 创建记录及字段默认值相关约束

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
 )
 
-// UserInfo --> 数据表
-type UserInfo struct {
-	ID uint
+// 定义模型
+
+type User struct {
+	// 内嵌 gorm.Model
+	gorm.Model // 继承 id ， 创建时间， 更新时间 删除时间字段
 	Name string
-	Gender string
-	Hobby string
+	Age sql.NullInt64
+	Birthday *time.Time
+	Email string `gorm:"type:varchar(100);unique_index"` // 唯一约束
+	Role string `gorm:"size:255"` // 设置字段大小为 255
+	MemberNumber *string `gorm:"unique;not null"` // 设置会员号 唯一且不为空
+	Num int `gorm:"AUTO_INCREMENT"` // 设置 字段为自增类型
+	Address string `gorm:"index:addr"` // 给 address 字段创建名为 addr 的索引
+	IgnoreMe int `gorm:"-"` // 忽略本字段
+}
+
+// 使用 AnimalID 作为主键 默认生成的表名复数 Animals
+
+type Animal struct {
+	AnimalID int64 `gorm:"primary_key;column:beast_id"` // gorm 自定义列名
+	Name 	 string `gorm:"column:day_of_beast"` // gorm 自定义列名
+	Age      int64 `gorm:"column:age_of_the_beast"`
+}
+
+// 重命名表名 将 Animal 表名转换成 vincent
+
+func (Animal)TableName() string{
+	return "vincent"
 }
 
 func main(){
@@ -22,26 +45,15 @@ func main(){
 		panic(err)
 	}
 	defer db.Close()
+	db.SingularTable(true) // 禁用表名复数
 
 	// 创建表 自动迁移 (把结构体和数据表进行对应)
-	//db.AutoMigrate(&UserInfo{})
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Animal{})
 
-	//// 创建数据行
-	//u1 := UserInfo{1, "提米", "男", "play"}
-	//db.Create(&u1)
+	// 使用 User 结构体创建表名叫 xiaowangzi 的表
+	db.Table("xiaowangzi").CreateTable(&User{})
 
-	// 查询
-	var u UserInfo
-	db.First(&u) // 查询表中的第一条数据 保存到 u 中
-	fmt.Printf("%#v\n", u) //main.UserInfo{ID:0x1, Name:"提米", Gender:"男", Hobby:"play"}
-	fmt.Println(u.ID, u.Name, u.Gender, u.Hobby)
-
-	// 更新
-	db.Model(&u).Update("hobby", "篮球")
-	fmt.Println(u.ID, u.Name, u.Gender, u.Hobby) // 1 提米 男 篮球
-
-	// 删除
-	db.Delete(&u)
 
 
 
